63362473f3f25de2fa0ae5ea9209c5c7c4e223d2
bogdankol <68349689+bogdankol@users.noreply.github.com>
2024-03-25 20:01:01 +0200

    parent ce6041917089f67b45a557c32c7c78a7a93034bb author bogdankol <68349689+bogdankol@users.noreply.github.com> 1711389661 +0200 committer bogdankol <68349689+bogdankol@users.noreply.github.com> 1713526627 +0300
    asd, could be transformed into complex commit
    
    complex commit333
    
    second commit to be squashed
    
    third commit to be squashed
    
    complex commit222
    
    third commit to be squashed

---
+++ app/ui/search.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation'
+import { useDebouncedCallback } from 'use-debounce';
+import { useEffect } from 'react';
+
+export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams)
+    params.set('page', '1')
+    if(!term) {
+      params.delete('query')
+    } else {
+      params.set('query', term)
+    }
+    const params2 = new URLSearchParams(searchParams)
+    const string = `${pathname}?${params.toString()}`
+    
+
+    replace(string)
+    console.log({
+      searchParams,
+      params,
+      string,
+      paramsProcessed: params2.get('query')
+    })
+  }, 500)
+
+  useEffect(() => {
+    console.log(1111111111111111)
+  })
+
+
+  return (
+    <div className="relative flex flex-1 flex-shrink-0">
+      <label htmlFor="search" className="sr-only">
+        Search
+      </label>
+      <p>{Math.random()}</p>
+      <input
+        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        placeholder={placeholder}
+        onChange={e => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
+      />
+      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+    </div>
+  );
+}
